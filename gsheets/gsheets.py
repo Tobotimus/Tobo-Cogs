a85821d97a7014334b0b98fbcc15f377ca7450b9
Tobotimus <tobyharradine@live.com>
2017-06-10 12:35:46 +1000

    [GSheets] Requests are asynchronous
    Most HTTP requests are done using aiohttp. Still using httplib2 for
    authorization though (i.e. getting access tokens) however this is ok
    for now, since a new instance is being passed in for every request.

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -1,15 +1,16 @@
 import os
 import re
+import logging
+import urllib3
 import httplib2
 import discord
 from discord.ext import commands
 from cogs.utils import checks
 from cogs.utils.chat_formatting import pagify, box
 from cogs.utils.dataIO import dataIO
-from googleapiclient import discovery
-from googleapiclient.errors import HttpError
-from oauth2client import client, tools
+from oauth2client import client, tools, transport
 from oauth2client.file import Storage
+from aiohttp import ClientSession
 from argparse import ArgumentParser
 try:
     from tabulate import tabulate
@@ -22,7 +23,7 @@ CREDENTIAL_PATH = "{}/credentials.json".format(FOLDER_PATH)
 FLOW_KWARGS = {
     "client_id": "681019046420-g62edq8nu52adniue2400g8eus64a9e7.apps.googleusercontent.com",
     "client_secret": "38xBpRxw44h7-eZcS9kHHZVn",
-    "scope": 'https://www.googleapis.com/auth/spreadsheets',
+    "scope": 'https://www.googleapis.com/auth/spreadsheets.readonly',
     "redirect_uri": client.OOB_CALLBACK_URN,
     "user_agent": "DiscordBot",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -31,9 +32,37 @@ FLOW_KWARGS = {
 CHANNEL = "channel"
 SERVER = "server"
 GLOBAL = "global"
+_LOGGER = logging.getLogger('gsheets')
 
-class InvalidSheetsURL(Exception):
+class GSheetsError(Exception):
+    """Base exception for this cog."""
     pass
+
+class InvalidSheetsURL(GSheetsError):
+    """URL does not point to a google sheet"""
+    pass
+
+class HttpError(GSheetsError):
+    """HTTP data was invalid or unexpected"""
+    def __init__(self, resp, content):
+        self.resp = resp
+        if not isinstance(content, dict):
+            raise TypeError("HTTP content should be dict")
+        self.content = content
+
+    def _get_reason(self):
+        """Calculate the reason for the error from the response content."""
+        reason = self.resp.reason
+        try:
+            reason = self.content['error']['message']
+        except (ValueError, KeyError, TypeError):
+            pass
+        return reason
+
+    def __repr__(self):
+        return '<HttpError %s "%s">' % (self.resp.status, self._get_reason())
+
+    __str__ = __repr__
 
 class GSheets:
     """Display data from Google Sheets in discord!
@@ -78,7 +107,7 @@ class GSheets:
             await self.bot.say("There is already a sheet with that name in your scope.")
             return
         try:
-            s_id = self.gc.check_url(url)
+            s_id = await self.gc.check_url(url)
         except InvalidSheetsURL:
             await self.bot.say("That doesn't look like a valid URL.")
             return
@@ -130,9 +159,9 @@ class GSheets:
         for range in ranges:
             try:
                 if not table:
-                    table = self.gc.get_range(sheet_id, range)
+                    table = await self.gc.get_range(sheet_id, range)
                 else:
-                    temp_table = self.gc.get_range(sheet_id, range)
+                    temp_table = await self.gc.get_range(sheet_id, range)
                     i = 0
                     for row in temp_table:
                         if i == len(table):
@@ -174,7 +203,8 @@ class GSheets:
         if resp:
             try:
                 code = resp.content
-                credentials = flow.step2_exchange(code)
+                http = httplib2.Http()
+                credentials = flow.step2_exchange(code, http=http)
             except client.FlowExchangeError as e:
                 await self.bot.say("Authentication has failed: {}".format(e.args[0]))
                 return
@@ -229,15 +259,20 @@ class GSheets:
         return credentials
 
 class GSheetsClient:
+    """Interface to get data from google sheets.
 
-    def __init__(self, credentials):
-        http = credentials.authorize(httplib2.Http())
-        discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                        'version=v4')
-        self.service = discovery.build('sheets', 'v4', http=http, 
-                                       discoveryServiceUrl=discoveryUrl)
+    This module should be thread-safe. Currently, all 
+    requests are done using the aiohttp library except for 
+    refreshing access tokens, which is done using a new 
+    instance of httplib2.Http for every request. (getting 
+    the initial access token isn't done in this class). 
+    """
 
-    def check_url(self, url):
+    def __init__(self, credentials: client.OAuth2Credentials):
+        self.credentials = credentials
+        self.session = ClientSession()
+
+    async def check_url(self, url):
         """Checks if a URL points to a valid spreadsheet, 
         which can be accessed by the client. 
 
@@ -253,13 +288,13 @@ class GSheetsClient:
         _url_key_re_v2 = re.compile(r"/spreadsheets/d/([a-zA-Z0-9-_]+)")
         key = _url_key_re_v1.search(url)
         if key:
-            return self.check_key(key.group(1))
+            return await self.check_key(key.group(1))
         key = _url_key_re_v2.search(url)
         if key:
-            return self.check_key(key.group(1))
+            return await self.check_key(key.group(1))
         raise InvalidSheetsURL()
 
-    def check_key(self, key):
+    async def check_key(self, key):
         """Checks if the client can access the sheet with the given 
         key. 
 
@@ -269,14 +304,39 @@ class GSheetsClient:
         Returns:
             The spreadsheet's ID.
         """
-        self.service.spreadsheets().get(
-            spreadsheetId=key).execute()
-        return key
+        url = "https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}"
+        resp = await self.get(url.format(spreadsheetId=key))
+        if resp.status == 200:
+            return key
+        else:
+            raise HttpError(resp=resp, content=await resp.json())
 
-    def get_range(self, id: str, range: str):
-        result = self.service.spreadsheets().values().get(
-            spreadsheetId=id, range=range).execute()
-        return result.get('values', [])
+    async def get_range(self, id: str, range: str):
+        url = "https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}?majorDimension=ROWS"
+        resp = await self.get(url.format(spreadsheetId=id, range=range))
+        if resp.status == 200:
+            body = await resp.json()
+            return body.get('values', [])
+        else:
+            raise HttpError(resp=resp, content=await resp.json())
+
+    async def request(self, method, url: str):
+        """Perform an authorized http request. Applies 
+        access token from credentials to headers and refreshes 
+        access token if needed.
+
+        Token refresh is done using a httplib2.Http() instance. 
+        This is still thread-safe because a new instance is being 
+        passed in for a single optional request.
+        """
+        headers = {}
+        access_token = self.credentials.get_access_token(httplib2.Http()).access_token
+        headers['Authorization'] = 'Bearer ' + access_token
+        return await self.session.request(method, url, headers=headers)
+
+    async def get(self, url: str):
+        """Perform an authorized http GET request."""
+        return await self.request('GET', url)
 
 def check_folders():
     if not os.path.exists(FOLDER_PATH):
