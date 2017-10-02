6b6c7f0505467639f86b9a03c70c244e5a315893
Tobotimus <tobyharradine@live.com>
2017-10-02 11:41:32 +1100

    [Admin] Spice up autorole

--- toboadmin/autorole.py
+++ toboadmin/autorole.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands
 from redbot.core import Config, checks
+from redbot.core.utils.chat_formatting import box
 
 __all__ = ["UNIQUE_ID", "Autorole"]
 
@@ -19,31 +20,63 @@ class Autorole:
     @commands.group()
     @checks.admin_or_permissions(manage_role=True)
     async def autorole(self, ctx: commands.Context):
-        """Manage Autorole settings"""
+        """Manage Autorole settings.
+
+        I must have the Manage Roles permission in order for this to work."""
         if not ctx.invoked_subcommand:
             await ctx.bot.send_cmd_help(ctx)
+            settings = self.conf.guild(ctx.guild)
+            role = await settings.role()
+            if role is not None:
+                role = discord.utils.get(ctx.guild.roles, id=role)
+            if role is None:
+                role = "Not set"
+            await ctx.send(
+                box("Enabled: {!s}\n"
+                    "Role: {!s}"
+                    "".format(await settings.enabled(), role)))
 
     @autorole.command(name="setrole")
+    @commands.bot_has_permissions(manage_roles=True)
     async def _autorole_set(self, ctx: commands.Context, *,
                             role: discord.Role):
         """Set the role."""
+        if role > ctx.guild.me.top_role:
+            await ctx.send("I cannot apply roles which are above me in the"
+                           " role hierarchy.")
+            return
         await self.conf.guild(ctx.guild).role.set(role.id)
         await ctx.send("Done.")
 
     @autorole.command(name="toggle")
+    @commands.bot_has_permissions(manage_roles=True)
     async def _autorole_toggle(self, ctx: commands.Context):
         """Toggle Autorole on or off."""
-        enabled = await self.conf.guild(ctx.guild).enabled()
-        await self.conf.guild(ctx.guild).enabled.set(not enabled)
-        await ctx.send("Done.")
+        guild = ctx.guild
+        role = await self.conf.guild(guild).role()
+        role = discord.utils.get(guild.roles, id=role)
+        if role is None:
+            await ctx.send("The role is not set - see `{}autorole setrole`."
+                           "".format(ctx.prefix))
+            return
+        enabled = not await self.conf.guild(guild).enabled()
+        await self.conf.guild(guild).enabled.set(enabled)
+
+        await ctx.send("Done. The `{!s}` role will {} be applied when a"
+                       " new member joins."
+                       "".format(role, "now" if enabled else "no longer"))
 
     async def on_member_join(self, member: discord.Member):
         """Fires when a member joins and gives them the role, if enabled."""
         guild = member.guild
-        settings = await self.conf.guild(guild).all()
-        if not settings["enabled"] or settings["role"] is None:
+        enabled = await self.conf.guild(guild).enabled()
+        role_id = await self.conf.guild(guild).role()
+        if not enabled or role_id is None:
             return
-        role = next((r for r in guild.roles if r.id == settings["role"]), None)
+        role = discord.utils.get(guild.roles, id=role_id)
         if role is None:
             return
-        await member.add_roles(role)
+        try:
+            await member.add_roles(role)
+        except discord.HTTPException:
+            pass
