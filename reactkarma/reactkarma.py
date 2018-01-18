902abc687041d18e5fc0369a9a86715a75061ea2
bobloy <alboblexloy@gmail.com>
2018-01-18 11:12:21 -0500

    Nick not nickname

--- reactkarma/reactkarma.py
+++ reactkarma/reactkarma.py
@@ -347,7 +347,7 @@ class ReactKarma():
     def _get_embed(self, message: discord.Message):
         upvote = self._get_emoji(message.server, UPVOTE)
         embed=discord.Embed(title="{} **{}** | Found in {}".format(upvote, self.topkarma[message.id]["KARMA"], message.channel.mention) , description=message.content)
-        embed.set_author(name=message.author.nickname, icon_url=message.author.avatar_url)
+        embed.set_author(name=message.author.nick, icon_url=message.author.avatar_url)
         if message.attachments:
             if imghdr.what(message.attachments[0].url) in ['gif','jpeg','png','webp']:
                 embed.set_image(url=message.attachments[0].url)
