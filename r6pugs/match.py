bb0876eef52ab765732269519cfbc65dd6a96a44
Tobotimus <Tobotimus@users.noreply.github.com>
2018-04-27 18:03:57 +1000

    [V3] Added license at the bottom of all python modules (#17)

--- r6pugs/match.py
+++ r6pugs/match.py
@@ -79,3 +79,25 @@ class PugMatch:
     def has_member(self, member: discord.Member):
         """Check if a member is a part of one of this match's teams."""
         return any(member in (p1, p2) for p1, p2 in zip(*self.teams))
+
+
+'''Copyright (c) 2017, 2018 Tobotimus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
--- welcomecount/welcomecount.py
+++ welcomecount/welcomecount.py
@@ -101,8 +101,7 @@ class WelcomeCount:
         for channel in guild.channels:
             if await self.conf.channel(channel).enabled():
                 welcome_channels.append(channel)
-        
-        last_message = None
+
         for channel in welcome_channels:
             channel_settings = self.conf.channel(channel)
             if not new_day:
@@ -120,3 +119,25 @@ class WelcomeCount:
             welcome = await channel_settings.welcome_msg()
             msg = await channel.send(welcome.format(**params))
             await channel_settings.last_message.set(msg.id)
+
+
+'''Copyright (c) 2017, 2018 Tobotimus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
